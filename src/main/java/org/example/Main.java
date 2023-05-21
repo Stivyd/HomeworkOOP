111173b1f27725f4eb6546f071090d7d83342c83
Тетерин Дмитрий QAMID-61 <teterin130215@yandex.ru>
2023-05-21 17:45:58 +0500

    Edited commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Post post = new Post();
         post.name = "Иван";
-        post.passport = "4444 № 44444444";
+        post.passSerial = 444 ;
+        post.passNumber  = 4444444 ;
         post.patronymic = "Иванович";
         post.phone = "+7(999) 999-99-99";
         post.surname = "Иванов";
-        post.subscription = String.valueOf(true);
+        post.subscription = true;
 
         post.birthday = new FormDate();
         post.birthday.day = 13;
         post.birthday.month = 12;
         post.birthday.year = 2001;
 
+        if (post.subscription == true) {
+            System.out.println("Согласие получено");
+        } else {
+            System.out.println("Согласие не получено");
+        }
 
+
+        System.out.println("Имя      - " + post.name);
+        System.out.println("Отчество - " + post.patronymic);
+        System.out.println("Фамилия  - " + post.surname);
+        System.out.println("Телефон  - " + post.phone);
+        System.out.println("Дата рождения: " + post.birthday.day + "." + post.birthday.month + "." + post.birthday.year + "г");
+        System.out.println("Паспорт:  серия " + post.passSerial + " номер " + post.passNumber);
     }
 }
--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -2,12 +2,15 @@ package org.example;
 
 public class Post {
     public String name;
-    public String passport;
-    public String patronymic;
-    public String phone;
     public String surname;
-    public String subscription;
+    public String patronymic;
+    public int passport;
 
+    public String phone;
+    public boolean subscription;
+    public int passSerial;
+    public int passNumber;
     FormDate birthday = new FormDate();
+
 
 }
